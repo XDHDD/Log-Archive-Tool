9ae603256a618f8831a6b407b71bd8edcce06d17
Ilia <ilakatysev@gmail.com>
2025-02-08 21:07:32 +0000

    fix default behavior

--- log-archive.sh
+++ log-archive.sh
@@ -6,6 +6,7 @@ tar_ext=".tar.gz" # default extension
 compress_type="z" # default comprassion
 delimiter=""  # default delimiter
 verbose="" # default no output of archive content
+custom_target=0  # var if -p given
 
 # main func
 main(){
@@ -43,9 +44,10 @@ main(){
   date_hms="%H$delimiter%M$delimiter%S" # time format like hour, minute, second
   timestamp="$(date +"$date_ymd"_"$date_hms")" # default timestamp
   full_name="log_archive_$timestamp$tar_ext" # full name of backup file
-  file_size=$(stat -c "%s" "$full_name" | numfmt --to=iec) # calculating backup size
+
 
   tar -c"$verbose""$compress_type"f "$full_name" -C "$target_path" . # creating tar archive with comprassion
+  file_size=$(stat -c "%s" "$full_name" | numfmt --to=iec) # calculating backup size
   mv log_archive_"$timestamp""$tar_ext" "$backup_path" # copy to backup directory
   echo "Created backup file $full_name, $file_size" | tee logger # show output of executed script
 }
@@ -57,7 +59,7 @@ help() {
   cat << EOF
 log-archive is utility for making backups to /var/backup path with timestamp.
 
-Usage: log-archive <options> <value>
+Usage: log-archive <options> <value> <target>
 
 Options:
   -h,             Show this help
@@ -75,14 +77,13 @@ Options:
 
 Examples:
 
-  1.Default usage: log-archive or log-archive '/var/log'
-     Result: /var/backup/log_archive_2025-02-08_08-18-45.tar.gz
+  1.Default usage: log-archive
 
-  2.Override comprassion type with custom delimiter: log-archive -c J -d '#'
-     Result: /var/backup/log_archive_2025#02#08_08#19#59.tar.xz
+  2.Run log-archive with target: log-archive /var/log/apt or log-archive -c j /var/log/apt or log-archive -c j -p /var/log/apt 
 
-  3.Make a backup of /var/log/messages: log-archive -p /var/log/messages
-     Result: /var/backup/log_archive_2025-02-08_13-39-47.tar.gz
+  3.Override comprassion type with custom delimiter: log-archive -c J -d '#'
+
+  4.Make a backup of /var/log/messages: log-archive -c z /var/log/messages
 
 EOF
 }
@@ -108,12 +109,13 @@ while getopts ":c:hp:d:v" opt; do
   esac
 done
 
-if [[ -z "$1" ]]; then
-  echo "No passed directory"
-  exit 1
-else
-  target_path=$1
-fi
 
+# Shift processed options
+shift $((OPTIND - 1))
+
+# If there is no -p key, take first positional arg
+if [[ $custom_target -eq 0 && -n "$1" ]]; then
+    target_path="$1"
+fi
 echo $1
 main
